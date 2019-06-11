92e9e123dd61e69053f556ff9fdb0aad273f0d6a
valentin Lecoq-Vallon <vlecoq-v@student.42.fr>
2019-06-11 12:07:36 +0200

    trying to connect to local db

--- config/database.php
+++ config/database.php
@@ -1,5 +1,6 @@
 <?php
 $DB_SET = "mysql:host=127.0.0.1;";
-$DB_DSN = "mysql:host=127.0.0.1;dbname=camagru";
+// $DB_DSN = "mysql:host=127.0.0.1;dbname=camagru";
+$DB_DSN = "mysql:host=127.0.0.1:3306/index.php;dbname=camagru;";
 $DB_USER = 'root';
 $DB_PASSWORD = 'Jano78';
